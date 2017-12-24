b2ab5677ab7eb9c037c61bfced7af56ca967095e
xueheng <2811860293@qq.com>
2017-12-24 12:44:42 +0800

    测试提交

--- app/src/main/java/com/xueheng/hellodemo/LoginActivity.java
+++ app/src/main/java/com/xueheng/hellodemo/LoginActivity.java
@@ -19,6 +19,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
---
+++ app/src/main/java/com/xueheng/hellodemo/utils/ToastUtil.java
@@ -0,0 +1,9 @@
+package com.xueheng.hellodemo.utils;
+
+/**
+ * Created by Administrator on 2017/12/24.
+ */
+
+public class ToastUtil {
+    
+}
