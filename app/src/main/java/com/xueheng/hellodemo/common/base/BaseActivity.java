1d5d7f7db100787c5763f476999a965013c25a81
xueheng <2811860293@qq.com>
2017-12-24 21:14:48 +0800

    封装日志类

---
+++ app/src/main/java/com/xueheng/hellodemo/common/base/BaseActivity.java
@@ -0,0 +1,95 @@
+package com.xueheng.hellodemo.common.base;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+
+/**
+ * Created by xueheng on 2017/12/24.
+ */
+
+public abstract class BaseActivity extends AppCompatActivity implements View.OnClickListener{
+
+
+
+    public abstract void initData();
+
+    public abstract void initView();
+
+    protected <T extends View> T findView(int id) {
+        return (T) findViewById(id);
+    }
+
+    /**
+     * 隐藏软件盘
+     */
+    public void hideSoftInput() {
+        InputMethodManager imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+        if (getCurrentFocus() != null) {
+            imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+        }
+    }
+
+    /**
+     * 显示软键盘
+     */
+    public void showInputMethod(){
+        if (getCurrentFocus() != null){
+            InputMethodManager imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+            imm.showSoftInputFromInputMethod(getCurrentFocus().getWindowToken(),0);
+        }
+    }
+
+    private boolean fastClick() {
+        long lastClick = 0;
+        if (System.currentTimeMillis() - lastClick <= 1000) {
+            return false;
+        }
+        lastClick = System.currentTimeMillis();
+        return true;
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+
+    public void startActivity(Class<?> clz) {
+        startActivity(clz, null);
+    }
+
+    /**
+     *携带数据的页面跳转
+     * @param clz
+     * @param bundle
+     */
+    public void startActivity(Class<?> clz, Bundle bundle) {
+        Intent intent = new Intent();
+        intent.setClass(this, clz);
+        if (bundle != null) {
+            intent.putExtras(bundle);
+        }
+        startActivity(intent);
+    }
+
+    /**
+     * 含有Bundle通过Class打开编辑界面
+     *
+     * @param cls
+     * @param bundle
+     * @param requestCode
+     */
+    public void startActivityForResult(Class<?> cls, Bundle bundle,
+                                       int requestCode) {
+        Intent intent = new Intent();
+        intent.setClass(this, cls);
+        if (bundle != null) {
+            intent.putExtras(bundle);
+        }
+        startActivityForResult(intent, requestCode);
+    }
+}
