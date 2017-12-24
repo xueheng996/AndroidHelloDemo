de3b29bef74ddc791c3e2bc6576f9e462a752190
xueheng <2811860293@qq.com>
2017-12-24 13:13:25 +0800

    测试提交

--- app/src/main/java/com/xueheng/hellodemo/module/login/LoginActivity.java
+++ app/src/main/java/com/xueheng/hellodemo/module/login/LoginActivity.java
@@ -1,4 +1,4 @@
-package com.xueheng.hellodemo;
+package com.xueheng.hellodemo.module.login;
 
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
@@ -19,7 +19,6 @@ import android.os.Build;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -29,6 +28,8 @@ import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import com.xueheng.hellodemo.R;
 
 import java.util.ArrayList;
 import java.util.List;
